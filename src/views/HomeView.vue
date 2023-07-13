0f9d4fbc2a4e44b88062cbd600af79ee29111bdc
iraklikeche <irakli.kechee@gmail.com>
2023-07-13 17:40:12 +0400

    profleView component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <script setup>
 import AuthModal from "../components/AuthModal.vue";
+import ProfileView from "./ProfileView.vue";
+
 import { ref } from "vue";
 
-const isAuth = ref(false);
+const isAuth = ref(true);
+const isLogin = ref(false);
 </script>
 
 <template>
-  <div class="welcome">
+  <div class="welcome" v-if="isLogin">
     <h1>Welcome To {{ isAuth ? "Entertainment App" : "Your Profile" }}</h1>
     <div class="sign-btns" v-if="isAuth">
       <AuthModal :isLogin="false" />
       <AuthModal :isLogin="true" />
     </div>
+  </div>
+  <div v-else>
+    <ProfileView />
   </div>
 </template>
 
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { ref } from "vue";
+import logo from "@/assets/logo.svg";
+import home from "@/assets/icon-nav-home.svg";
+import movies from "@/assets/icon-nav-movies.svg";
+import tv from "@/assets/icon-nav-tv-series.svg";
+import bookmark from "@/assets/icon-nav-bookmark.svg";
+import search from "@/assets/icon-search.svg";
+
+const value = ref("");
+</script>
+
+<template>
+  <div class="container">
+    <div class="side">
+      <img :src="logo" class="logo" />
+
+      <div class="nav-icons">
+        <a href="#">
+          <img :src="home" class="icon-nav" />
+        </a>
+        <a class="icon-nav" href="#">
+          <img :src="movies" />
+        </a>
+        <a class="icon-nav" href="#">
+          <img :src="tv" />
+        </a>
+        <a class="icon-nav" href="#">
+          <img :src="bookmark" />
+        </a>
+      </div>
+    </div>
+
+    <main>
+      <div class="search">
+        <img :src="search" />
+        <a-input
+          v-model:value="value"
+          placeholder="Search for movies or TV series"
+          class="input"
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  padding-top: 20px;
+}
+
+.search {
+  display: flex;
+  gap: 10px;
+}
+.container {
+  max-width: 1200px;
+  padding: 20px 40px;
+  display: flex;
+  gap: 50px;
+}
+
+.side {
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 25px;
+  background-color: #161d2f;
+  border-radius: 5px;
+  height: 90vh;
+  gap: 80px;
+}
+
+.input {
+  background-color: #10141e;
+  border: none;
+  color: #fff;
+  font-weight: bold;
+}
+
+.logo {
+  width: 30px;
+}
+
+.icon-nav {
+  width: 20px;
+}
+
+.nav-icons {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+}
+/* 
+.logo:hover,
+.icon-nav:hover {
+  color: #fc4747;
+} */
+
+a.icon-nav:hover {
+  color: #fc4747;
+}
+</style>
